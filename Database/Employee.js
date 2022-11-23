fcb3376247e93003ba8443dcc1ae09ccce1dd7d7
Ryan-Jones1999 <ryan.jones@kainos.com>
2022-11-23 11:32:22 +0000

    Updated file structure and installed various testing dependencies - added tests for view job service class

--- Database/Employee.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios'); 
-
-
-exports.viewjobroles = async () => {
-    const results = await axios.get('http://localhost:8080/api/viewjobroles')
-
-    return results
-}
--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const nunjucks = require('nunjucks');
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 3000;                  //Save the port number where your server will be listening
-const employee = require('./Database/Employee')
-nunjucks.configure('Pages', {
+const employee = require('./app/Service/Employee')
 
+nunjucks.configure('./app/Pages', {
     express: app
-
 });
 
 app.set('view engine', 'njk');
 
 app.get('/viewroles',  async (req, res) =>  {
     var s = await employee.viewjobroles()
-    res.render('viewroles', { roles: s.data })
+    res.render('viewroles', { roles: s })
 })
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
